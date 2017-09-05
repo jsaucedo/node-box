80d29e6fee556069d75ab1085bd58760a6e523cf
Juan Ignacio Saucedo <jsaucedo.dev@gmail.com>
2017-09-05 17:05:13 -0300

    update yarn installation command

--- cookbooks/equinauts/attributes/nodejs.rb
+++
@@ -1,6 +0,0 @@
-default['nodejs']['version'] = '0.8.6'
-default['nodejs']['checksum'] = 'dbd42800e69644beff5c2cf11a9d4cf6dfbd644a9a36ffdd5e8c6b8db9240854'
-default['nodejs']['dir'] = '/usr/local'
-default['nodejs']['src_url'] = "http://nodejs.org/dist"
-
-#6.11.2 1ca74833ff79e6a3a713a88bba8e7f5f5cda5d4008a6ffeb2293a1bf98f83e04
